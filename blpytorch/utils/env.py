dbc0538c8fe329a89a86892d06b3fbe478ca4ed1
Beilong <bt132@duke.edu>
2024-11-22 21:25:37 +0800

    update

--- blpytorch/utils/env.py
+++ blpytorch/utils/env.py
@@ -1,3 +1,16 @@
+import yaml
+
+def get_env(config_path:str):
+    """
+    config_path: str to yaml config
+    """
+    with open(config_path, "r") as f:
+        config = yaml.safe_load(config_path)
+    config = AttrDict(**config)
+    return config
+
+
+
 class AttrDict(dict):
     def __init__(self, *args, **kwargs):
         super(AttrDict, self).__init__(*args, **kwargs)
@@ -8,3 +21,9 @@ class AttrDict(dict):
             return super().__getattribute__(name)
         except AttributeError:
             return None
+
+if __name__ == "__main__":
+    config = yaml.safe_load("a: 2")
+    config = AttrDict(**config)
+    print(config.a)
+    print(config.get("a"))
