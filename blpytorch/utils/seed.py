1b60540475f8b966e18cd3d886a4683f7edf96bf
Beilong Tang <btang5@ncsu.edu>
2026-07-28 16:53:08 -0400

    update [auto]

--- blpytorch/utils/seed.py
+++ blpytorch/utils/seed.py
@@ -8,6 +8,6 @@ def setup_seed(seed, rank):
     np.random.seed(SEED)
     torch.manual_seed(SEED)
     torch.cuda.manual_seed_all(SEED)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+    # torch.backends.cudnn.deterministic = True
+    # torch.backends.cudnn.benchmark = False
     return SEED
