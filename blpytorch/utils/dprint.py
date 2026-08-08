2f2c7565290dcadd471b26bf866d9056be3e43a8
Beilong Tang <btang5@ncsu.edu>
2026-08-08 13:22:05 -0400

    update [auto]

--- blpytorch/utils/dprint.py
+++ blpytorch/utils/dprint.py
@@ -1,6 +1,11 @@
+import os
+
 printed_dict={}
 
-def dprint(msg:str, name=None):
+def dprint(msg:str, name=None, test = False):
+    if test:
+        if os.getenv('DPRINT_TEST') in [None, '0', 'true']:
+            return
     if name is None:
         name = msg
     if name not in printed_dict:
